a2ebf0635875500adf12094d9662cfa864062690
vieiragustavo25-oss <vieira.gustavo25@escola.pr.gov.br>
2026-09-13 18:54:37 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+/*JavaScript basico basic #easy bleep feito por mim :p*/
+function myFunction() {
+  document.getElementById("demo").innerHTML = "Parabéns, vc perdeu 1 segundo da sua vida com isso ❤";
+}
+
+/*Funcionalidade Extra - IA*/
+const botaoTema = document.querySelector(".tema");
+
+botaoTema.addEventListener("click", function() {
+    document.body.classList.toggle("modo-noturno");
+
+    if (document.body.classList.contains("modo-noturno")) {
+        botaoTema.innerHTML = "☀️ Modo normal";
+    } else {
+        botaoTema.innerHTML = "🌙 Modo noturno";
+    }
+});
